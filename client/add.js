95f56b66ab52f485bc899cc9afbb1b9453435a86
Michal Wanat <michalwanat@gmail.com>
2015-03-19 18:09:39 +0100

    cleaning code, readme

--- client/add.js
+++ client/add.js
@@ -2,7 +2,7 @@
 
 Template.universeGalleryAdd.created = function () {
     var template = this;
-    template.gallery_id = new ReactiveVar('bArSR9ks9CMs5jXbZ');
+    template.gallery_id = new ReactiveVar();
 };
 
 Template.universeGalleryAdd.helpers({
@@ -19,15 +19,9 @@ Template.universeGalleryAdd.events({
         var $form = $(template.find('form'));
         $form.submit();
     },
-    'change input.js-universeGalleryId': function (e, template) {
-        var $el = $(e.target);
-        var gallery_id = $el.val();
-        template.gallery_id.set(gallery_id);
-    },
     'click .js-remove-photo': function (e, template) {
         var $el = $(e.target);
-        var $input = $(template.find('.js-universeGalleryId'));
-        var gallery_id = $input.val();
+        var gallery_id = template.gallery_id.get();
         var file_id = $el.attr('data-id') ? $el.attr('data-id') : $el.parent().attr('data-id');
 
         UniGallery.removeFileFromGallery(gallery_id, file_id);
@@ -36,21 +30,23 @@ Template.universeGalleryAdd.events({
 
 AutoForm.addHooks(['UniGalleryGalleries'], {
     onSuccess: function (formType, result, template) {
-        var $input_file = $(template.find('input[type=file]'));
-        var $input_gallery_id = $input_file.parents('.js-universeGalleryAdd').find('input.js-universeGalleryId');
-        var gallery_id = $input_gallery_id.val();
-        var file_priv_id = result;
+        var parent_template = UniUtils.getParentTemplateInstance('universeGalleryAdd', template);
+        var gallery_id = parent_template.gallery_id.get();
+        var input_id = template.data.input_id;
+        var $input_gallery_id = $('input#'+input_id);
+        var file_id = result;
 
         if(!gallery_id){
             UniGallery.Galleries.insert({
-                files: [file_priv_id]
+                files: [file_id]
             }, function (error, result) {
                 gallery_id = result;
+                parent_template.gallery_id.set(gallery_id);
                 $input_gallery_id.val(gallery_id).change();
             });
         }else{
             UniGallery.Galleries.update(gallery_id, {
-                $push: {files: file_priv_id}
+                $push: {files: file_id}
             });
         }
     }
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Universe Gallery
+
+Content Management System for Universe Framework
+
+
+> This package is part of Universe, a framework based on [Meteor platform](http://meteor.com)
+maintained by [Vazco](http://www.vazco.eu).
+
+> It works standalone, but you can get max out of it when using the whole system.
+
+
+
+### 1. Create input hidden with unique id. Gallery id will set in this input after adding file.
+```
+<input type="hidden" name="gallery_id" id="uniqueGalleryId" />
+```
+
+OR
+
+```
+{{> afQuickField type="hidden" name="gallery_id" id="uniqueGalleryId"}}
+```
+
+
+### 2. Put this unique Id of input to parameter "input_id". This template need include outside any autoform block. 
+
+```
+{{> universeGalleryAdd input_id="uniqueGalleryId"}}
+```
+
+### 3. If you would like to add own opening modal button, you have to set "hide_button" param to "true" and create own button.
+
+```
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="modal_uniqueGalleryId">
+    Add gallery
+</button>
+{{> universeGalleryAdd input_id="uniqueGalleryId" hide_button="true"}}
+```
+
+### 4. To edit gallery you need include template "universeGalleryEdit" with "gallery_id" param.
+
+```
+{{> universeGalleryEdit gallery_id=gallery_id}}
+```
+
+### 5. To show gallery you need include template "universeGalleryView" with "gallery_id" param.
+
+```
+{{> universeGalleryView gallery_id=gallery_id}}
+```
